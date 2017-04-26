2d4ddd7f1a6eddb16b28e15398af0858dd6705ca
Frederico de Oliveira <fredericoliveira@gmail.com>
2017-04-26 15:21:31 -0300

    Sharing Default Project Files.

---
+++ app/src/main/java/br/com/a3maismais/a321go/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.a3maismais.a321go;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
