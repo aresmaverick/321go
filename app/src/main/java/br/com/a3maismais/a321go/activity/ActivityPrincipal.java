884f7581998e0f15f755c5b21b60ffe54495eed0
Frederico de Oliveira <fredericoliveira@gmail.com>
2017-05-02 19:26:55 -0300

    Change to a more significant class name.

--- app/src/main/java/br/com/a3maismais/a321go/activity/ActivityPrincipal.java
+++ app/src/main/java/br/com/a3maismais/a321go/activity/ActivityPrincipal.java
@@ -1,13 +1,15 @@
-package br.com.a3maismais.a321go;
+package br.com.a3maismais.a321go.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import br.com.a3maismais.a321go.R;
+
+public class ActivityPrincipal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.principal);
     }
 }
