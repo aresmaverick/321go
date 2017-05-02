b714eee29d20870ff6e2834115d484987d3ea653
Frederico de Oliveira <fredericoliveira@gmail.com>
2017-05-02 19:27:46 -0300

    Created a activity class for the settings screen.

---
+++ app/src/main/java/br/com/a3maismais/a321go/activity/ActivitySettings.java
@@ -0,0 +1,10 @@
+package br.com.a3maismais.a321go.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by fred on 02/05/17.
+ */
+
+public class ActivitySettings extends AppCompatActivity {
+}
