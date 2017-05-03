9f0092e8f5b645c225f36f1657b5665aafda455d
Frederico de Oliveira <fredericoliveira@gmail.com>
2017-05-03 10:48:34 -0300

    First class version.

--- app/src/main/java/br/com/a3maismais/a321go/activity/ActivitySettings.java
+++ app/src/main/java/br/com/a3maismais/a321go/activity/ActivitySettings.java
@@ -1,10 +1,21 @@
 package br.com.a3maismais.a321go.activity;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import br.com.a3maismais.a321go.R;
 
 /**
  * Created by fred on 02/05/17.
  */
 
 public class ActivitySettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
 }
