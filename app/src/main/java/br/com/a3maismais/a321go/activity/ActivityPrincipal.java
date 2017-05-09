e8a396908ceb71b9e5632d8d9c88078972dca822
Frederico de Oliveira <fredericoliveira@gmail.com>
2017-05-09 11:49:20 -0300

    Preparing the class to receive the CountDown class.

--- app/src/main/java/br/com/a3maismais/a321go/activity/ActivityPrincipal.java
+++ app/src/main/java/br/com/a3maismais/a321go/activity/ActivityPrincipal.java
@@ -1,15 +1,14 @@
 package br.com.a3maismais.a321go.activity;
 
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Chronometer;
 import android.widget.GridLayout;
 import android.widget.TextView;
 
 import br.com.a3maismais.a321go.R;
+import br.com.a3maismais.a321go.model.CountDown;
 import br.com.a3maismais.a321go.model.RoutineConfig;
 
 public class ActivityPrincipal extends AppCompatActivity {
@@ -21,9 +20,12 @@ public class ActivityPrincipal extends AppCompatActivity {
     TextView executedExerciseTime;
     TextView executedRestTime;
 
+    TextView minutesText;
+    public TextView secondText;
+    TextView centisecondText;
+
     GridLayout background;
 
-    private Chronometer chronometer;
     boolean isPaused = false;
     long savedTime = 0;
 
@@ -36,38 +38,26 @@ public class ActivityPrincipal extends AppCompatActivity {
         Button settingButton = (Button) findViewById(R.id.settings_button);
         Button pauseButton = (Button) findViewById(R.id.pause_button);
 
-        chronometer = (Chronometer) findViewById(R.id.chronometer);
-
         numberExecutedCicles = (TextView) findViewById(R.id.number_executed_cicles);
         numberExecutedExercises = (TextView) findViewById(R.id.number_executed_exercises);
         executedExerciseTime = (TextView) findViewById(R.id.executed_exercise_time);
         executedRestTime = (TextView) findViewById(R.id.executed_rest_time);
 
+        minutesText = (TextView) findViewById(R.id.minutes_text);
+        secondText = (TextView) findViewById(R.id.second_text);
+        centisecondText = (TextView) findViewById(R.id.centisecond_text);
+
         background = (GridLayout) findViewById(R.id.background_chron);
 
         config = (RoutineConfig) getIntent().getSerializableExtra(RoutineConfig.CONFIG_NAME);
 
+        final CountDown cd = new CountDown(this, 99000, 1000);
+
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                //FIXME - Não esta recebendo valores da tela
-                //FunctionalRoutine fr = new FunctionalRoutine(ActivityPrincipal.this);
-                //fr.start();
-
-                if(isPaused){
-                    chronometer.setBase(SystemClock.elapsedRealtime() + savedTime);
-                    chronometer.start();
-                    savedTime = 0;
-                    isPaused = false;
-                }
-                else{
-                    chronometer.setBase(SystemClock.elapsedRealtime()); //executedExerciseTime
-                    chronometer.start();
-                    savedTime = 0;
-                }
-
-
+                secondText.setText("99");
+                cd.start();
             }
         });
 
@@ -81,11 +71,7 @@ public class ActivityPrincipal extends AppCompatActivity {
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isPaused == false){ //entra para false;
-                    savedTime = chronometer.getBase() - SystemClock.elapsedRealtime();
-                }
-                chronometer.stop();
-                isPaused = true;
+
             }
         });
     }
